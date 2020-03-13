d3665135ac70c634190058e5be836bbc0fe57ef1
FapconV <chakkavenu@gmail.com>
2020-03-13 20:08:55 +0530

    made some forms

---
+++ src/SignUp.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class SignUp {
+    private JTextField nametextfield;
+    private JTextField mobiletextfield;
+    private JTextField emailtextfield;
+    private JTextField usernametextfield;
+    private JPasswordField ConfirmPassword;
+    private JPasswordField Password;
+    private JCheckBox showPasswordCheckBox;
+    private JCheckBox showPasswordCheckBox1;
+    private JButton confirmButton;
+    private JPanel Registration;
+}
